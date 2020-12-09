12cf991442a0b6043944646c12b095f5e572dfe4
Damien Garaud <damien.garaud@gmail.com>
2020-12-09 12:00:46 +0100

    day-01: refactoring
    add a core.clj with the 'read-expenses' function

--- 2020/clojure/src/day_01/part_one.clj
+++ 2020/clojure/src/day_01/part_one.clj
@@ -1,31 +1,34 @@
 (ns day-01.part-one
-  (:require [clojure.string :as str]))
+  (:require [day-01.core :refer [read-expenses]]))
 
 
-(defn read-expenses []
-  "Read the input expenses and return a list of ints."
-  (map #(Integer/parseInt %)
-       (-> (slurp "src/day_01/input")
-         (str/split #"\n"))))
-
-
-(defn find-sum [left right]
-  "Find the the pair where the sum is equal to 2020. Suppose left and right are sorted"
+(defn find-sum [left right target]
+  "Find the the pair where the sum is equal to `target`. Suppose left and right are sorted"
   (let [minv (first left)
         maxv (first right)
+        minsize (min (count left) (count right))
         sum (+ minv maxv)]
     (cond
-      (= sum 2020) (list minv maxv)
-      (< sum 2020) (find-sum (rest left) right)
-      (> sum 2020) (find-sum left (rest right))
-      )))
+      (or (= minsize 1) (= minv maxv)) nil
+      (= sum target) (list minv maxv)
+      (< sum target) (find-sum (rest left) right target)
+      (> sum target) (find-sum left (rest right) target))))
+
+
+(defn display-solution [solution]
+  (let [[left right] solution]
+    (println "Pair where sum=2020: " (list left right))
+    (println "Solution: " (* left right))
+    (* left right)))
 
 
 (defn run [opts]
-  "clj -X day_01.core/run"
+  "clj -X day-01.part-one/run"
   (let [expenses (read-expenses)
+        target 2020
         asc (sort < expenses)
         desc (sort > expenses)
-        [left right] (find-sum asc desc)]
-    (println "Pair where sum=2020: " (list left right))
-    (println "Solution: " (* left right))))
+        solution (find-sum asc desc target)]
+    (if
+        (nil? solution) (println "no solution")
+        (display-solution solution))))
