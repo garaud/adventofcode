667f9634324485c87c679706b9f11d14f4ce7eb7
Damien Garaud <damien.garaud@gmail.com>
2022-12-04 12:00:03 +0100

    2022/day_04: part one

---
+++ 2022/julia/day_04/runtests.jl
@@ -0,0 +1,11 @@
+using Test
+
+include("part_one.jl")
+
+
+@testset "Check the number of assignment where one contains the other" begin
+    data = readinput("test.txt")
+    @test count_assignment_pairs(data) == 2
+end
+
+
