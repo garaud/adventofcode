51d1ad50b4789365246714859b9964354ba2e736
Damien Garaud <damien.garaud@gmail.com>
2024-04-05 00:42:45 +0200

    2023/day-02: start part one

---
+++ 2023/elixir/lib/day02.ex
@@ -0,0 +1,22 @@
+defmodule Advent.Day02 do
+@moduledoc """
+Day 02 - Cube Conundrum
+
+You've arrived at Snow Island. You met a Elf who wanted to play a game with you
+with a bag of cubes. The cubes can be in red, blue or green.
+
+All possible games for a specific number of red, blue and green cube is the result of this puzzle
+"""
+
+
+  def solve_part_one(fname) do
+    content = Advent.read_input("02", fname)
+     8
+  end
+
+
+  def main() do
+    result = solve_part_one("inputs")
+    IO.puts("result part-one: #{result}")
+  end
+end
---
+++ 2023/elixir/test/advent_day02_test.exs
@@ -0,0 +1,7 @@
+defmodule AdventDay02Test do
+  use ExUnit.Case
+
+  test "part one" do
+    assert Advent.Day02.solve_part_one("test-part-one") == 8
+  end
+end
